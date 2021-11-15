ec59b5558c8dd4e25fd42ca9711dbc13ea760fa1
Michael DeSantis <m.desantis628@gmail.com>
2021-11-14 22:13:30 -0500

    working on getting weather with api

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+const results = document.querySelector('.results');
+const input = document.querySelector('input');
+const form = document.forms[0];
+
+async function getWeather(zip) {
+  const response = await fetch(
+    'api.openweathermap.org/data/2.5/weather?zip=' +
+      zip +
+      ',us&appid=53dcc962829731a4fa033950e8997254',
+    { mode: 'cors' }
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getWeather(`${input.value}`);
+});
